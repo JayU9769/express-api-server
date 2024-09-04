bd05ebf72ccfe9f7affbe13b547cdebc9c1a624b
Jay Parmar <parmarjaydeep9769@gmail.com>
2024-09-04 10:51:14 +0530

    - Prisma and Seeder changes done

--- src/seeders/userSeeder.ts
+++ src/seeders/userSeeder.ts
@@ -1,17 +1,24 @@
 import { faker } from '@faker-js/faker';
-import {User} from "@/models/user.model";
+import { PrismaClient } from '@prisma/client';
 
 export async function seed(count: number) {
+  const prisma = new PrismaClient();
   const users = [];
   const status = ['active', 'inactive'];
+
   for (let i = 0; i < count; i++) {
     users.push({
       name: faker.person.fullName(),
       email: faker.internet.email(),
       password: faker.internet.password(),
-      status: status[Math.floor(Math.random() * status.length)]
+      status: status[Math.floor(Math.random() * status.length)],
     });
   }
-  await User.bulkCreate(users);
+
+  await prisma.user.createMany({
+    data: users,
+  });
+
   console.log(`${count} users have been seeded successfully.`);
+  await prisma.$disconnect(); // Close the Prisma client connection
 }
