438a80a791dd7eed64acd99cfabbd0fbf0cc282b
Jay Parmar <parmarjaydeep9769@gmail.com>
2024-09-14 21:27:08 +0530

    -

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,6 @@
 import { config } from 'dotenv';
 config({});
 
-export const dbConfig = {
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  database: process.env.MYSQL_DATABASE,
-  username: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
-}
-
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 
 export const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
