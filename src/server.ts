08970f42a52d34ba6476b682e53266c5e53a1fda
Jay Parmar <parmarjaydeep9769@gmail.com>
2024-10-02 17:47:41 +0530

    - Local commit

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import { UserRoute } from '@/routes/user.route'; // Route for user-related endpo
 import { AdminRoute } from '@/routes/admin.route'; // Route for admin-related endpoints
 import { RoleRoute } from '@/routes/role.route';
 import {PermissionRoute} from "@/routes/permission.route"; // Route for role-related endpoints
+import serverless from 'serverless-http';
 
 // Validate that required environment variables are set
 ValidateEnv();
@@ -23,5 +24,5 @@ const app = new App([
   new UserRoute(), // User-related API routes
 ]);
 
-// Start the application and listen on the configured port
-app.listen();
+// Export the wrapped Express app as a serverless function
+module.exports.handler = serverless(app.app);
